16198ce70c79c7ecfda3103aa37963c3f6010840
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-05-18 15:49:22 -0700

    Create Interests.js

---
+++ components/Interests.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Interests(){
+    return(
+        <div className="interest-div">
+            <h3 className="interests-h3">
+            Interests
+            </h3>
+            <p className="interests-p">
+            The 3 C’s: Cats, Cooking, Coding. I love yoga and I enjoy a good run! Music is my happy place.
+            </p>
+        </div>
+    )
+}
