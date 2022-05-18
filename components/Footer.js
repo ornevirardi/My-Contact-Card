1f32682aebf1ecb5e9fdf483b02769c7b4f0da0a
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-05-18 15:48:43 -0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Footer (){
+    return(
+        <div className="footer-div">
+            <img className="img-tw" src="./images/tw.png"/>
+            <img className="img-fb" src="./images/fb.png"/>
+            <img className="img-ig" src="./images/ig.png"/>
+            <img className="img-gh" src="./images/gh.png"/>
+        </div>
+    )
+}
