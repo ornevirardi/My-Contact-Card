b3bf78c1ad53f4885390c77054634595046b8362
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-05-18 15:48:24 -0700

    Create About.js

---
+++ components/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function About(){
+    return(
+        <div className="about-div">
+            <h3 className="about-h3">
+            About
+            </h3>
+            <p className="about-p">
+            I am a frontend developer, I am interested in building a great experience for the user. I am always happy to learn, always looking for new ways of improving my work. Challenge is my friend
+            </p>
+        </div>
+    )
+}
